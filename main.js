398b3c3dec430f9c6d242d1cf78fe2241bf51c4f
Felipe Antunes <felipeantunes@MacBook-Air-de-Felipe.local>
2023-09-10 10:35:08 -0300

    finish simple version

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('oba')
+const $ = (search) => document.querySelector(search);
+const textarea = $('#text')
+const words = $('#words')
+const charsWithSpace = $('#chars-with-space')
+const charsWithoutSpace = $('#chars-without-space')
+
+const updateField = (field, value) => {
+    field.innerHTML = value;
+}
+
+const findWords = (str) => str.trim().split(' ').filter(Boolean);
+const findWordsLength = (str) => findWords(str).length;
+const findCharsWithSpace = (str) => str.length;
+const findCharsWithoutSpace = (str) => findWords(str).map(word => word.length).reduce((prev, current) => prev + current, 0)
+
+const handleChangeTextArea = ({target: { value }}) => {
+    updateField(words, findWordsLength(value))
+    updateField(charsWithSpace, findCharsWithSpace(value))
+    updateField(charsWithoutSpace, findCharsWithoutSpace(value))
+}
+
+textarea.addEventListener('keyup', handleChangeTextArea)
